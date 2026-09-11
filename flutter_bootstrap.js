f3737c44308b2af9f142f91f17c81ab90480c643
piedcipher <piedcipher@users.noreply.github.com>
2026-09-11 13:24:47 +0000

    deploy: 983ac06d38a160e85a10590c756add216f06f8e7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"4e12c1aad286279cd6c8b6be8cb9968cbf473b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "408166857" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1967444131" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
